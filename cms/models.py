a657496deedb71432946c88981cc1235b0841859
nadeeraka <nadeerakan@gmail.com>
2020-05-16 00:52:43 +0530

    init 🚀

--- cms/models.py
+++ cms/models.py
@@ -6,23 +6,19 @@ from django.db import models
 
 # Create your models here.
 
-class Client(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, unique=True)
-    email = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100, unique=True)
-
-    class Meta:
-        ordering = ['id']
+class AppUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='app_user')
+    email = models.EmailField()
 
 
 class Income(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     note = models.CharField(max_length=100, blank=True)
     name = models.ForeignKey("IncomeType", default='', on_delete=models.CASCADE, related_name="incomes")
     amount = models.DecimalField(max_digits=100, decimal_places=4, default=0)
-    timestamp = models.DateTimeField(default=datetime.now, blank=True)
+    created_time = models.DateTimeField(default=datetime.now, blank=True)
+    update_time = models.DateTimeField(default=datetime.now, blank=True)
 
     class Meta:
         ordering = ['id']
@@ -30,11 +26,12 @@ class Income(models.Model):
 
 class Expense(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     note = models.CharField(max_length=100, blank=True)
     name = models.ForeignKey("ExpenseType", on_delete=models.CASCADE, related_name="expenses")
     amount = models.DecimalField(max_digits=100, decimal_places=4, default=0)
-    timestamp = models.DateTimeField(default=datetime.now, blank=True)
+    created_time = models.DateTimeField(default=datetime.now, blank=True)
+    update_time = models.DateTimeField(default=datetime.now, blank=True)
 
     class Meta:
         ordering = ['id']
@@ -42,58 +39,37 @@ class Expense(models.Model):
 
 class Saving(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     note = models.CharField(max_length=100, blank=True)
-    name = models.ForeignKey('SavingType', on_delete=models.CASCADE, related_name="savings")
+    selection = models.ForeignKey('SavingType', on_delete=models.CASCADE, related_name="savings")
     amount = models.DecimalField(max_digits=100, decimal_places=4, default=0)
-    timestamp = models.DateTimeField(default=datetime.now, blank=True)
+    created_time = models.DateTimeField(default=datetime.now, blank=True)
     rate = models.DecimalField(max_digits=100, decimal_places=4, default=0)
+    update_time = models.DateTimeField(default=datetime.now, blank=True)
 
     class Meta:
         ordering = ['id']
 
 
 class ExpenseType(models.Model):
-    EXPENSE_CHOICES = [
-        ('L', 'Lifestyle'),
-        ('E', 'Entertainment'),
-        ('D', 'Donation'),
-        ('ED', 'Eduction'),
-        ('O', 'Other'),
-        ('F', 'Fixed'),
-        ('EQ', 'Equipment'),
-
-    ]
-    name = models.CharField(max_length=2, default='Salary', choices=EXPENSE_CHOICES)
-
     id = models.AutoField(primary_key=True)
+    selection_type = models.CharField(max_length=100)
 
     class Meta:
         ordering = ['id']
 
 
 class SavingType(models.Model):
-    SAVING_CHOICES = [
-        ('B', 'Bank'),
-        ('O', 'Other'),
-    ]
-    name = models.CharField(max_length=2, default='Bank', choices=SAVING_CHOICES)
     id = models.AutoField(primary_key=True)
+    selection_type = models.CharField(max_length=100)
 
     class Meta:
         ordering = ['id']
 
 
 class IncomeType(models.Model):
-    INCOME_CHOICES = {
-        ('S', 'Salary'),
-        ('P', 'Property'),
-        ('B', 'Business'),
-        ('O', 'Other')
-    }
-
-    name = models.CharField(max_length=2, default='Salary', choices=INCOME_CHOICES)
     id = models.AutoField(primary_key=True)
+    selection_type = models.CharField(max_length=100)
 
     class Meta:
         ordering = ['id']
@@ -101,9 +77,11 @@ class IncomeType(models.Model):
 
 class Balance(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=100, blank=True)
     amount = models.DecimalField(max_digits=100, decimal_places=4, default=0)
+    update_time = models.DateTimeField(default=datetime.now, blank=True)
+    created_time = models.DateTimeField(default=datetime.now, blank=True)
 
     class Meta:
         ordering = ['id']
