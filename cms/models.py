89ba7a4610f0ca83de339064d853286ef2e2eb78
nadeeraka <nadeerakan@gmail.com>
2020-05-16 00:29:13 +0530

    init 🚀

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,109 @@
+from datetime import datetime
+from typing import List, Tuple
+from django.contrib.auth.models import User, Group
 from django.db import models
 
+
 # Create your models here.
+
+class Client(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['id']
+
+
+class Income(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    note = models.CharField(max_length=100, blank=True)
+    name = models.ForeignKey("IncomeType", default='', on_delete=models.CASCADE, related_name="incomes")
+    amount = models.DecimalField(max_digits=100, decimal_places=4, default=0)
+    timestamp = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        ordering = ['id']
+
+
+class Expense(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    note = models.CharField(max_length=100, blank=True)
+    name = models.ForeignKey("ExpenseType", on_delete=models.CASCADE, related_name="expenses")
+    amount = models.DecimalField(max_digits=100, decimal_places=4, default=0)
+    timestamp = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        ordering = ['id']
+
+
+class Saving(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    note = models.CharField(max_length=100, blank=True)
+    name = models.ForeignKey('SavingType', on_delete=models.CASCADE, related_name="savings")
+    amount = models.DecimalField(max_digits=100, decimal_places=4, default=0)
+    timestamp = models.DateTimeField(default=datetime.now, blank=True)
+    rate = models.DecimalField(max_digits=100, decimal_places=4, default=0)
+
+    class Meta:
+        ordering = ['id']
+
+
+class ExpenseType(models.Model):
+    EXPENSE_CHOICES = [
+        ('L', 'Lifestyle'),
+        ('E', 'Entertainment'),
+        ('D', 'Donation'),
+        ('ED', 'Eduction'),
+        ('O', 'Other'),
+        ('F', 'Fixed'),
+        ('EQ', 'Equipment'),
+
+    ]
+    name = models.CharField(max_length=2, default='Salary', choices=EXPENSE_CHOICES)
+
+    id = models.AutoField(primary_key=True)
+
+    class Meta:
+        ordering = ['id']
+
+
+class SavingType(models.Model):
+    SAVING_CHOICES = [
+        ('B', 'Bank'),
+        ('O', 'Other'),
+    ]
+    name = models.CharField(max_length=2, default='Bank', choices=SAVING_CHOICES)
+    id = models.AutoField(primary_key=True)
+
+    class Meta:
+        ordering = ['id']
+
+
+class IncomeType(models.Model):
+    INCOME_CHOICES = {
+        ('S', 'Salary'),
+        ('P', 'Property'),
+        ('B', 'Business'),
+        ('O', 'Other')
+    }
+
+    name = models.CharField(max_length=2, default='Salary', choices=INCOME_CHOICES)
+    id = models.AutoField(primary_key=True)
+
+    class Meta:
+        ordering = ['id']
+
+
+class Balance(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, blank=True)
+    amount = models.DecimalField(max_digits=100, decimal_places=4, default=0)
+
+    class Meta:
+        ordering = ['id']
---
+++ cms/urls.py
@@ -0,0 +1,29 @@
+# from django.urls import  path , include
+# from app import views
+# from rest_framework import routers
+#
+# router = routers.DefaultRouter()
+# router.register(r'todos', views.TodoViewSet)
+# router.register(r'notesCollection', views.NoteCollectionViewSet)
+# router.register(r'todoCollection', views.TodoCollectionViewSet)
+# router.register(r'goal', views.GoalViewSet)
+# router.register(r'note', views.NoteViewSet)
+#
+#
+#
+# urlpatterns = [
+#        path('', include(router.urls)),
+# ]
+
+#
+# urlpatterns = [
+#     path('current_user/', views.current_user),
+#     path('userss/', views.UserList.as_view()),
+#     path('userProfile/', views.UserProfile.as_view()),
+#     path('delayQuantity/', views.DelayQuantity.as_view()),
+#     path('plantUsage/', views.PlantUsage.as_view()),
+#     path('taktTime/', views.TaktTimeView.as_view()),
+#     path('testView/', views.TestView.as_view()),
+#     path('', include(router.urls)),
+#     re_path(r'ws/test/$', consumers.TestConsumer),
+# ]
